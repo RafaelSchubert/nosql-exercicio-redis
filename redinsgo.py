c6c238e1e3ed2ce417d2150be281daceb55ad0c1
Rafael S. Campos <rafael.schubert.campos@gmail.com>
2020-04-05 23:10:07 -0300

    Carregar os números do bingo.
    Armazenar as "pedras" num conjunto (set) do Redis.

--- redinsgo.py
+++ redinsgo.py
@@ -5,13 +5,32 @@ REDIS_CONEXAO_HOST = 'localhost'
 REDIS_CONEXAO_PORT = 6379
 
 
+# Significa [1, 100), ou o mesmo que [1, 99].
+BINGO_NUMEROS_INTERVALO = (1, 100)
+
+
+CHAVE_BINGO_NUMEROS = 'bingo:numeros'
+
+
 conexao_redis = None
 
 
-def main():
-    with redis.Redis(REDIS_CONEXAO_HOST, REDIS_CONEXAO_PORT) as conexao_redis:
-        pass
+def carregar_numeros_bingo():
+    global conexao_redis
 
+    print(f'O jogo usará pedras numeradas de {BINGO_NUMEROS_INTERVALO[0]} a {BINGO_NUMEROS_INTERVALO[1] - 1}.')
+
+    conexao_redis.sadd(CHAVE_BINGO_NUMEROS, *range(*BINGO_NUMEROS_INTERVALO))
+
+
+def main():
+    global conexao_redis
+
+    print('REDINSGO: Bingo com Redis')
+    print()
+
+    with redis.Redis(REDIS_CONEXAO_HOST, REDIS_CONEXAO_PORT) as conexao_redis:
+        carregar_numeros_bingo()
 
 if __name__ == '__main__':
     main()
